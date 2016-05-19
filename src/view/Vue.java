1f54c837ac9abb1685465196fc59d32822db8e0a
bakhah <frkhourdin@gmail.com>
2016-05-19 17:04:37 +0200

    suppression toString

--- src/view/Vue.java
+++ src/view/Vue.java
@@ -11,14 +11,12 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
-import javax.swing.border.Border;
 import models.Etat;
 import models.Parcelle;
 import models.Piegeur;
 import models.Renifleur;
 import models.Topographe;
 import models.Zone;
-import sun.java2d.pipe.DrawImage;
 
 /**
  *
@@ -37,16 +35,13 @@ public class Vue extends JPanel
     Image renibleu = Toolkit.getDefaultToolkit().createImage("img/renibleu.png");
     Image piegrouge = Toolkit.getDefaultToolkit().createImage("img/piegrouge.png");
     Image piegbleu = Toolkit.getDefaultToolkit().createImage("img/piegbleu.png");
-    
+
     Image rock = Toolkit.getDefaultToolkit().createImage("img/rock.png");
     Image arbre = Toolkit.getDefaultToolkit().createImage("img/arbre.png");
     Image sanctubleu = Toolkit.getDefaultToolkit().createImage("img/sanctubleu.png");
     Image sancturouge = Toolkit.getDefaultToolkit().createImage("img/sancturouge.png");
     Image tas = Toolkit.getDefaultToolkit().createImage("img/tas.png");
     Image trou = Toolkit.getDefaultToolkit().createImage("img/trou.png");
-    
-   
-    
 
     public Vue() //Constructeur test
     {
@@ -56,36 +51,20 @@ public class Vue extends JPanel
 
         setBorder(BorderFactory.createLineBorder(Color.black));
     }
-    
+
     public void setZone(Zone zone)
     {
         this.zone = zone;
     }
+
     public void setVisible(boolean b)
     {
         this.isVisible = b;
     }
+
     public void setFog(boolean b)
     {
         this.isFog = b;
-    }
-    
-    @Override
-    public String toString() //Affiche une version txt de la Vue
-    {
-        StringBuilder res = new StringBuilder();
-        if (this.zone.getPerso() == null)
-        {
-            res.append("\033[0m");
-        }
-        else if (this.zone.getPerso().getEquipe() == 1)
-            res.append("\033[31m");
-        else
-            res.append("\033[34m");
-        
-        res.append(this.zone.getEtat().toString());
-        
-        return res.toString();
     }
 
     @Override
@@ -93,45 +72,61 @@ public class Vue extends JPanel
     {
         super.paintComponent(g); //To change body of generated methods, choose Tools | Templates.
         g.drawImage(background, 0, 0, this);
-        
-        
-        System.out.println("pouf");
-        
+
         if (this.zone.getEtat() != Etat.VIDE)
         {
             if (this.zone.getEtat() == Etat.ARBRE)
+            {
                 g.drawImage(arbre, 0, 0, null);
+            }
             if (this.zone.getEtat() == Etat.TROU)
+            {
                 g.drawImage(trou, 0, 0, null);
+            }
             if (this.zone.getEtat() == Etat.ROCHE)
+            {
                 g.drawImage(rock, 0, 0, null);
+            }
             if (this.zone.getEtat() == Etat.TAS)
+            {
                 g.drawImage(tas, 0, 0, null);
-            
+            }
+
         }
-        
+
         if (this.zone.contientPerso())
         {
             if (this.zone.getPerso().getEquipe() == 1)
             {
                 if (this.zone.getPerso() instanceof Topographe)
+                {
                     g.drawImage(toporouge, 0, 0, null);
+                }
                 if (this.zone.getPerso() instanceof Piegeur)
+                {
                     g.drawImage(piegrouge, 0, 0, null);
+                }
                 if (this.zone.getPerso() instanceof Renifleur)
+                {
                     g.drawImage(renirouge, 0, 0, null);
+                }
             }
             if (this.zone.getPerso().getEquipe() == 2)
             {
                 if (this.zone.getPerso() instanceof Topographe)
+                {
                     g.drawImage(topobleu, 0, 0, null);
+                }
                 if (this.zone.getPerso() instanceof Piegeur)
+                {
                     g.drawImage(piegbleu, 0, 0, null);
+                }
                 if (this.zone.getPerso() instanceof Renifleur)
+                {
                     g.drawImage(renibleu, 0, 0, null);
+                }
             }
         }
     }
-    
-    
+
 }
--- src/view/VueJoueur.java
+++ src/view/VueJoueur.java
@@ -40,23 +40,5 @@ public class VueJoueur extends JPanel
     {
         return this.tabVues[x][y];
     }
-    
-    @Override
-    public String toString() // Affiche une version txt de VueJoueur
-    {
-        StringBuilder str = new StringBuilder();
-        
-        for (int y = 0; y < 10; y++)
-        {
-            for (int x = 0; x < 10; x++)
-            {
-                str.append(tabVues[x][y].toString());
-                str.append(' ');
-            }
-            str.append('\n');
-        }
-        
-        return str.toString();
-    }
             
 }
