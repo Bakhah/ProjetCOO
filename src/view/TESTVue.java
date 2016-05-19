749649c699fe88b18559b027e95fa7c34b5df8a9
bakhah <frkhourdin@gmail.com>
2016-05-19 17:02:04 +0200

    Visuel fait (VueJoueur)

--- src/view/TESTVue.java
+++ src/view/TESTVue.java
@@ -5,8 +5,15 @@
  */
 package view;
 
+import java.awt.Dimension;
+import java.awt.Frame;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
 import models.Etat;
 import models.Parcelle;
+import models.Piegeur;
+import models.Renifleur;
 import models.Topographe;
 import models.Zone;
 
@@ -19,19 +26,46 @@ public class TESTVue
 
     public static void main(String[] args)
     {
+        VueJoueur vueJ = new VueJoueur();
         Zone avecJ1 = new Parcelle(Etat.VIDE);
         avecJ1.setPerso(new Topographe(1));
+        Zone avecJ1b = new Parcelle(Etat.TROU);
+        avecJ1b.setPerso(new Piegeur(1));
+        Zone avecJ2 = new Parcelle(Etat.VIDE);
+        avecJ2.setPerso(new Renifleur(2));
+        Zone avecJ2b = new Parcelle(Etat.VIDE);
+        avecJ2b.setPerso(new Topographe(2));
         
-        Zone avecJ2 = new Parcelle(Etat.TROU);
-        avecJ2.setPerso(new Topographe(2));
         
-        VueJoueur vueJ = new VueJoueur();
-        vueJ.getVue(5, 5).setZone(new Parcelle((Etat.TAS)));
-        vueJ.getVue(2, 2).setZone(new Parcelle((Etat.ARBRE)));
         
-        vueJ.getVue(1, 1).setZone(avecJ1);
-        vueJ.getVue(3, 5).setZone(avecJ2);
+        vueJ.getVue(5, 5).setZone(avecJ1);
+        vueJ.getVue(4, 5).setZone(avecJ1b);
+        vueJ.getVue(2, 7).setZone(avecJ2);
+        vueJ.getVue(5, 7).setZone(avecJ2b);
         
-        System.out.println(vueJ);
+        vueJ.getVue(0, 0).setZone(new Parcelle(Etat.ROCHE));
+        vueJ.getVue(0, 5).setZone(new Parcelle(Etat.TROU));
+        vueJ.getVue(1, 7).setZone(new Parcelle(Etat.TROU));
+        vueJ.getVue(3, 9).setZone(new Parcelle(Etat.ROCHE));
+        vueJ.getVue(2, 2).setZone(new Parcelle(Etat.ROCHE));
+        vueJ.getVue(4, 8).setZone(new Parcelle(Etat.ROCHE));
+        vueJ.getVue(2, 9).setZone(new Parcelle(Etat.ARBRE));
+        vueJ.getVue(1, 1).setZone(new Parcelle(Etat.ARBRE));
+        
+        
+        SwingUtilities.invokeLater(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                JFrame frame = new JFrame("test");
+                frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+                frame.setPreferredSize(new Dimension(500, 500));
+                frame.add(vueJ);
+                frame.pack();
+                frame.setVisible(true);
+            }
+        });
     }
+
 }
--- src/view/Vue.java
+++ src/view/Vue.java
@@ -5,26 +5,56 @@
  */
 package view;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
 import models.Etat;
 import models.Parcelle;
+import models.Piegeur;
+import models.Renifleur;
+import models.Topographe;
 import models.Zone;
+import sun.java2d.pipe.DrawImage;
 
 /**
  *
  * @author bakhah
  */
-public class Vue
+public class Vue extends JPanel
 {
 
     private Zone zone;
     private boolean isFog;
     private boolean isVisible;
+    Image background = Toolkit.getDefaultToolkit().createImage("img/bg.png");
+    Image toporouge = Toolkit.getDefaultToolkit().createImage("img/toporouge.png");
+    Image topobleu = Toolkit.getDefaultToolkit().createImage("img/topobleu.png");
+    Image renirouge = Toolkit.getDefaultToolkit().createImage("img/renirouge.png");
+    Image renibleu = Toolkit.getDefaultToolkit().createImage("img/renibleu.png");
+    Image piegrouge = Toolkit.getDefaultToolkit().createImage("img/piegrouge.png");
+    Image piegbleu = Toolkit.getDefaultToolkit().createImage("img/piegbleu.png");
+    
+    Image rock = Toolkit.getDefaultToolkit().createImage("img/rock.png");
+    Image arbre = Toolkit.getDefaultToolkit().createImage("img/arbre.png");
+    Image sanctubleu = Toolkit.getDefaultToolkit().createImage("img/sanctubleu.png");
+    Image sancturouge = Toolkit.getDefaultToolkit().createImage("img/sancturouge.png");
+    Image tas = Toolkit.getDefaultToolkit().createImage("img/tas.png");
+    Image trou = Toolkit.getDefaultToolkit().createImage("img/trou.png");
+    
+   
+    
 
     public Vue() //Constructeur test
     {
         zone = new Parcelle(Etat.VIDE);
         isFog = false;
         isVisible = true;
+
+        setBorder(BorderFactory.createLineBorder(Color.black));
     }
     
     public void setZone(Zone zone)
@@ -57,4 +87,51 @@ public class Vue
         
         return res.toString();
     }
+
+    @Override
+    protected void paintComponent(Graphics g)
+    {
+        super.paintComponent(g); //To change body of generated methods, choose Tools | Templates.
+        g.drawImage(background, 0, 0, this);
+        
+        
+        System.out.println("pouf");
+        
+        if (this.zone.getEtat() != Etat.VIDE)
+        {
+            if (this.zone.getEtat() == Etat.ARBRE)
+                g.drawImage(arbre, 0, 0, null);
+            if (this.zone.getEtat() == Etat.TROU)
+                g.drawImage(trou, 0, 0, null);
+            if (this.zone.getEtat() == Etat.ROCHE)
+                g.drawImage(rock, 0, 0, null);
+            if (this.zone.getEtat() == Etat.TAS)
+                g.drawImage(tas, 0, 0, null);
+            
+        }
+        
+        if (this.zone.contientPerso())
+        {
+            if (this.zone.getPerso().getEquipe() == 1)
+            {
+                if (this.zone.getPerso() instanceof Topographe)
+                    g.drawImage(toporouge, 0, 0, null);
+                if (this.zone.getPerso() instanceof Piegeur)
+                    g.drawImage(piegrouge, 0, 0, null);
+                if (this.zone.getPerso() instanceof Renifleur)
+                    g.drawImage(renirouge, 0, 0, null);
+            }
+            if (this.zone.getPerso().getEquipe() == 2)
+            {
+                if (this.zone.getPerso() instanceof Topographe)
+                    g.drawImage(topobleu, 0, 0, null);
+                if (this.zone.getPerso() instanceof Piegeur)
+                    g.drawImage(piegbleu, 0, 0, null);
+                if (this.zone.getPerso() instanceof Renifleur)
+                    g.drawImage(renibleu, 0, 0, null);
+            }
+        }
+    }
+    
+    
 }
